bb6301e29a7c01836b3b003bbe4bf44a1cb6d71e
Milan Navratil <navratil.milann@gmail.com>
2026-02-09 13:31:25 +0100

    Templates

--- src/lib/utils/blocks/Template1Block.ts
+++ src/lib/utils/blocks/Template1Block.ts
@@ -3,6 +3,7 @@ import { registerBlock } from "@/lib/utils/registry.ts";
 import { TextBlock } from "@/lib/utils/blocks/TextBlock.ts";
 import { ColumnBlock, ColumnChildren, ColumnOptions } from "@/lib/utils/blocks/ColumnBlock.ts";
 import { ImageBlock } from "@/lib/utils/blocks/ImageBlock.ts";
+import Template1Image from '@/assets/img/template_1.png'
 
 export class Template1 extends ColumnBlock implements Block {
     name: string = 'template_1';
@@ -14,7 +15,7 @@ export class Template1 extends ColumnBlock implements Block {
                 styleClass: 'col',
             },
             2: {
-                styleClass: 'col z-300',
+                styleClass: 'col',
             },
         },
     }
@@ -47,6 +48,7 @@ export class Template1 extends ColumnBlock implements Block {
     icon: string = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#4f39f6"><path d="M440-120H200q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h240v720Zm-80-80v-560H200v560h160Zm160-320v-320h240q33 0 56.5 23.5T840-760v240H520Zm80-80h160v-160H600v160Zm-80 480v-320h320v240q0 33-23.5 56.5T760-120H520Zm80-80h160v-160H600v160ZM360-480Zm240-120Zm0 240Z"/></svg>';
     title: string = 'Text a obrázok';
     type: BlockType = 'layout';
+    exampleImage?: string = Template1Image;
 
 }
 
